e0e96aefdaa51fbf5589f67a862d7d285fa32746
kartinocana <info@kartingocana.com>
2026-03-30 16:47:09 +0200

    Update db.js

--- db.js
+++ db.js
@@ -11,7 +11,7 @@ const pool = isCloud
   : new Pool({
       host: process.env.DB_HOST || "localhost",
       user: process.env.DB_USER || "postgres",
-      password: String(process.env.DB_PASSWORD),
+      password: String(process.env.DB_PASSWORD || ""),
       database: process.env.DB_NAME,
       port: Number(process.env.DB_PORT) || 5432,
     });
